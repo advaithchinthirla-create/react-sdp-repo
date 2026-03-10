9978e315fef48c7c4db6071b2b24e680ad846277
royal <2500031218@gmail.com>
2026-03-10 15:31:10 +0530

    modified MainNavBar.jsx

--- src/pages/MainNavBar.jsx
+++ src/pages/MainNavBar.jsx
@@ -15,7 +15,7 @@ const MainNavBar = () => {
       <div className="navbar-container">
         <div className="navbar-logo">
           <Link to="/" className="logo-link">
-           Event Management System - 2500031218
+           Event Management System - 2500031218 
           </Link>
         </div>
         <ul className="nav-menu">
